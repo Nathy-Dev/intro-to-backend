b9fd9b4f6c191a96045547051cf7ac2b9b54fff2
Nathaniel McGregor <nathanielmcgr@gmail.com>
2025-12-27 05:43:02 +0100

    chore: initialize project with package.json and essential dependencies

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import app from "./app.js";
+
+dotenv.config({
+    path: "./.env"      // how to load the .env file
+});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.on("error", (error) => {
+            console.log("Error starting the server:", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 5000}`);
+        })
+    } catch (error) {
+        console.log("Failed to start the server:", error);
+        process.exit(1);
+    }
+}
+
+startServer();
